9ad75b5ba865a8e233c14c1c5f7a9cd3cee37847
mary sht <shtyrkova06@gmail.com>
2025-04-08 11:08:26 +0300

    get structure

---
+++ PPP_24-25_4sem-master/1lab/server.py
@@ -0,0 +1,17 @@
+import os
+import json
+import socket
+import threading
+
+# Функция для рекурсивного получения структуры директории
+def get_directory_structure(root_dir):
+    structure = {}
+    for dirpath, dirnames, filenames in os.walk(root_dir):
+        relative_path = os.path.relpath(dirpath, root_dir)
+        if relative_path == '.':
+            relative_path = ''
+        structure[relative_path] = {
+            'directories': dirnames,
+            'files': filenames
+        }
+    return structure
